9148a5fc30c60f31172a5ec3f9173497ffe68482
MuhammadBilal <muhammadbilal20130@gmail.com>
2022-01-11 17:25:53 -0800

    Add toast and click in new Branch

--- app/src/main/java/com/example/temppp/MainActivity.java
+++ app/src/main/java/com/example/temppp/MainActivity.java
@@ -9,17 +9,25 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    Button btn,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn=findViewById(R.id.btn);
+        btn2.findViewById(R.id.btn2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(),"Git master Branch",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"New Branch",Toast.LENGTH_LONG).show();
             }
         });
 
