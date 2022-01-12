9613fc45c736600245dcb959c3ec0d3a4e2601ad
MuhammadBilal <muhammadbilal20130@gmail.com>
2022-01-11 17:07:49 -0800

    Add new Branch and chinging toast msg

--- app/src/main/java/com/example/temppp/MainActivity.java
+++ app/src/main/java/com/example/temppp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.temppp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"New Branch",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
